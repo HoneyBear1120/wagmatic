7d47c8438cca70da0237a5ab324d9ca53c32365d
IceFrogMate <ddmitry723@gmail.com>
2022-04-29 21:52:45 +1000

    first commit

---
+++ precache-manifest.caae253c11fa9700f4f0e7523fcc3a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81f4e84a0529facef3ee",
+    "url": "/static/css/main.c19b8a84.chunk.css"
+  },
+  {
+    "revision": "81f4e84a0529facef3ee",
+    "url": "/static/js/main.81f4e84a.chunk.js"
+  },
+  {
+    "revision": "833c5782700be2092671",
+    "url": "/static/css/1.f56d791e.chunk.css"
+  },
+  {
+    "revision": "833c5782700be2092671",
+    "url": "/static/js/1.833c5782.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f914eed33c2c9b6bbd3471d57fdb4f3e",
+    "url": "/index.html"
+  }
+];
